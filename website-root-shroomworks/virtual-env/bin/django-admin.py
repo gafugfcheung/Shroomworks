bd251c4d7b88604d6b6148c4f5cf1b0d8d2e43d8
kamilryszkowski <kamil@trueai.io>
2016-12-03 15:07:14 +0000

    changes

--- website-root-shroomworks/virtual-env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/vrgimael/Projects/software-engineering/website-root-shroomworks/virtual-env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
