d7a76ca740ca64352eae0e1e0f0b0e3d643be2e8
kamilryszkowski <kamil@trueai.io>
2016-12-03 14:56:46 +0000

    purge

--- basic/models.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals
-from django.utils import timezone
-
-from django.db import models
-
-class Pic(models.Model):
-    lat = models.FloatField(default=0.0)
-    lng = models.FloatField(default=0.0)
-    pub_date = models.DateTimeField(auto_now_add=True, auto_now=False)
